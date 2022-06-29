090efffcce7caed77dd7da3b9709d2d134e47ea4
ldy28 <108062744+ldy28@users.noreply.github.com>
2022-06-29 18:03:53 +0900

    0629

--- java0629/src/java0629/ex05.java
+++ java0629/src/java0629/ex05.java
@@ -1,5 +1,19 @@
 package java0629;
 
 public class ex05 {
-
+	public static void main(String[] args) {
+		for(int i=2;i<=1000;i++) {
+			int k=0;
+			for(int j=1;j<i;j++) {
+				
+				if(i%j==0) {
+					
+					k+=j; 
+					if(i==k) {
+						System.out.println(i);
+					}
+				}
+			}
+		}
+	}
 }
---
+++ java0629/src/java0629/heart.java
@@ -0,0 +1,21 @@
+package java0629;
+
+public class heart {
+	public static void main(String[] args) {
+		for(int i=2;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i*2;j++) {
+				System.out.print("*");
+			}
+			for(int j=1;j<=;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i*2;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
